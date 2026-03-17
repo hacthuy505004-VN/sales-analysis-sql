3137b962c9f9e3a778e4def96709e9ad574b6839
Trịnh Đình Văn <hacthuy505004@gmail.com>
2026-03-17 18:58:37 +0700

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO Customers (name) VALUES
+('Van'), ('Nam'), ('Linh');
+
+INSERT INTO Products (product_name, price) VALUES
+('Laptop', 1000),
+('Phone', 500),
+('Tablet', 300);
+
+INSERT INTO Sales (customer_id, product_id, quantity, sale_date) VALUES
+(1,1,1,'2024-01-01'),
+(2,2,2,'2024-01-02'),
+(1,3,3,'2024-01-03'),
+(3,1,1,'2024-01-04');
